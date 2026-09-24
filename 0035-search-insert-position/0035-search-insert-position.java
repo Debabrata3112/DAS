2ac5ea2c083a938ba4c545c57491478bf00e17c9
Debabrata Dolui <debabratadolui2004@gmail.com>
2026-09-24 20:50:26 +0530

    Time: 0 ms (100%), Space: 44.9 MB (12.53%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int index = nums.length;
+        int left=0;
+        int right =nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=target){
+                index = mid;
+                right = mid - 1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return index;
+    }
+}
